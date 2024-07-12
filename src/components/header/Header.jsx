68ef3bb2020ef18797aef908a36a5a038ce62c31
Eman Awadh <emanwdh@gmail.com>
2024-07-12 14:24:35 -0400

    Style: Add font mixins, Style a Basic Header with Components, on Mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,17 @@
+import BrainFlixLogo from '../../assets/Logo/BrainFlix-logo.svg'
+import './Header.scss'
+
+
 function Header() {
 
     return( 
     
     <header className="header section">
-       <img className="header__logo"/>
-       <input className="header__search" type = "text"></input>
-       <span className="header__avatar"></span>
+       <img className="header__logo" src ={BrainFlixLogo} />
+       <div className='header__search-bar'>
+          <input className="search-bar__search" type = "text"></input>
+          <span><span className="search-bar__avatar"></span></span>
+       </div>
        <button className="button button--upload" type="submit">Upload</button>
     </header>
   )
