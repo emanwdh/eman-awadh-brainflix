01ca086a9e38aeb9433206927a4e1b5d0d8987e3
Eman Awadh <emanwdh@gmail.com>
2024-07-13 15:12:37 -0400

    Style: Style Tablet Comment Form section, use absolute form input label positioning relative to comment form div, adjust width percentage for input field items

--- src/components/comment-form/CommentForm.jsx
+++ src/components/comment-form/CommentForm.jsx
@@ -4,12 +4,12 @@ import "./CommentForm.scss"
 export default function CommentForm({ data, VideoMain }) {
   return (
     <>
-      <form className="comment-form section">
+      <form className="comment-form">
         <span><span className="comment-form__avatar avatar"></span></span>
         <div className="comment-form__input-field">
           <label className="input-field__label">Join the Conversation</label>
-          <textarea className="input-field__textarea"></textarea>
-          <button className="comment-form__button" type="submit">Comment</button>
+          <textarea className="input-field__textarea" placeholder = "Add a new comment"></textarea>
+          <button className="comment-form__button input-field__button" type="submit">Comment</button>
         </div>
       </form>
     </>
