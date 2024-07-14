2e6c68f5cd65e16ad4ef9528ea0d34ecfe75504e
Eman Awadh <emanwdh@gmail.com>
2024-07-14 18:24:22 -0400

    Style: Adjust margin for tablet and mobile, remove excessive margins applied through section class, within components

--- src/App.jsx
+++ src/App.jsx
@@ -10,94 +10,86 @@ import MainCommentsSection from "./components/MainCommentsSection/MainCommentsSe
 import SideBarVideos from "./components/side-bar-videos/Side-Bar-Videos";
 
 function App() {
-
-  const [VideoList, setVideoList] = useState(SiteData.slice(1,SiteData.length));
+  const [VideoList, setVideoList] = useState(
+    SiteData.slice(1, SiteData.length)
+  );
   const [VideoMain, setMainVideo] = useState(SiteData[0]);
 
-  const MainVideoHandler = (id)=>{
-    const newMainVideo = SiteData.filter((video)=> video.id == id)
+  const MainVideoHandler = (id) => {
+    const newMainVideo = SiteData.filter((video) => video.id == id);
     console.log(newMainVideo[0]);
-    setMainVideo(VideoMain => newMainVideo[0]);
+    setMainVideo((VideoMain) => newMainVideo[0]);
     console.log(VideoMain);
-}
+  };
 
-
-  const MoveHandler  = (id) => {
-    const newArray = SiteData.filter((video) => video.id !== id)
+  const MoveHandler = (id) => {
+    const newArray = SiteData.filter((video) => video.id !== id);
     setVideoList(newArray);
     console.log(newArray);
     console.log(id);
     MainVideoHandler(id);
-  
-  }
-
-
-
-
-
+  };
 
   function relativeDate(date) {
-
-    const diff = Math.round ((new Date() - new Date(date))/ 1000);
-    const minute = 60; 
+    const diff = Math.round((new Date() - new Date(date)) / 1000);
+    const minute = 60;
     const hour = minute * 60;
-    const day = hour * 24; 
-    const week = day * 7; 
+    const day = hour * 24;
+    const week = day * 7;
     const month = day * 30;
     const year = month * 12;
 
-
-  if (diff < 30) {
-    return "just now";
-  } else if (diff < minute) {
-    return diff + " seconds ago";
-  } else if (diff < 2 * minute) {
-    return "a minute ago";
-  } else if (diff < hour) {
-    return Math.floor(diff / minute) + " minutes ago";
-  } else if (Math.floor(diff / hour) == 1) {
-    return "1 hour ago";
-  } else if (diff < day) {
-    return Math.floor(diff / hour) + " hours ago";
-  } else if (diff < day * 2) {
-    return "yesterday";
-  } else if (diff < week) {
-    return week + " days ago";
-  } else if (diff < month) {
-    return Math.floor(diff / week) + " weeks ago";
-  } else if (diff < year) {
-    return Math.floor(diff / month) + " months ago";
-  } else {
-    return Math.floor(diff / year) + " years ago";
+    if (diff < 30) {
+      return "just now";
+    } else if (diff < minute) {
+      return diff + " seconds ago";
+    } else if (diff < 2 * minute) {
+      return "a minute ago";
+    } else if (diff < hour) {
+      return Math.floor(diff / minute) + " minutes ago";
+    } else if (Math.floor(diff / hour) == 1) {
+      return "1 hour ago";
+    } else if (diff < day) {
+      return Math.floor(diff / hour) + " hours ago";
+    } else if (diff < day * 2) {
+      return "yesterday";
+    } else if (diff < week) {
+      return week + " days ago";
+    } else if (diff < month) {
+      return Math.floor(diff / week) + " weeks ago";
+    } else if (diff < year) {
+      return Math.floor(diff / month) + " months ago";
+    } else {
+      return Math.floor(diff / year) + " years ago";
+    }
   }
-
-
-};
 
   return (
     <>
       <Header />
-      <div className="main-section">
-        <div className="main-video-section">
-            <MainVideo 
-                      relativeDate={relativeDate}
-                      VideoList={VideoList}
-                      VideoMain={VideoMain}
-                      setMainVideo={setMainVideo}
-                        />
-            <MainCommentCounter 
-            VideoMain={VideoMain} />           
-            <CommentForm data={SiteData} 
-            VideoMain={VideoMain}/>
-            <MainCommentsSection data={SiteData}
-              relativeDate={relativeDate} 
-              VideoMain={VideoMain}/>
+      <div className="main-section section">
+        <div className="main-video-section section">
+          <MainVideo
+            relativeDate={relativeDate}
+            VideoList={VideoList}
+            VideoMain={VideoMain}
+            setMainVideo={setMainVideo}
+          />
+          <MainCommentCounter VideoMain={VideoMain} />
+          <CommentForm data={SiteData} VideoMain={VideoMain} />
+          <MainCommentsSection
+            data={SiteData}
+            relativeDate={relativeDate}
+            VideoMain={VideoMain}
+          />
         </div>
-        <div className="next-video-section">
-        <VideoSideBar data={SiteData} 
-        setVideoList={setVideoList}
-        VideoList={VideoList}
-        MoveHandler={MoveHandler}/>
+        <div className="next-video-section section">
+          <VideoSideBar
+            data={SiteData}
+            setVideoList={setVideoList}
+            VideoList={VideoList}
+            MoveHandler={MoveHandler}
+          />
         </div>
       </div>
     </>
--- src/components/main-video/Main-Video.jsx
+++ src/components/main-video/Main-Video.jsx
@@ -1,48 +1,54 @@
-import './Main-Video.scss'
-import likesIcon from '../../assets/Font/Icons/likes.svg'
-import viewsIcon from '../../assets/Font/Icons/views.svg'
+import "./Main-Video.scss";
+import likesIcon from "../../assets/Font/Icons/likes.svg";
+import viewsIcon from "../../assets/Font/Icons/views.svg";
 
+export default function MainVideo({
+  data,
+  relativeDate,
+  VideoList,
+  VideoMain,
+}) {
+  console.log(VideoList);
+  console.log(VideoMain);
 
-export default function  MainVideo ({data, relativeDate, VideoList, VideoMain}) {
-
-    console.log(VideoList);
-    console.log(VideoMain);
-
-return (
-
-    <div className="main-video section">
-        <video controls className="main-video__video" src = {VideoMain.video} poster = {VideoMain.image}></video>
-        <div className = "main-video__main-info main-info">
-            <h1 className="main-info__title">{VideoMain.title}</h1>
-            <div className="main-info__video-details">
-                <div className="video-details__stats">
-                    <div className="video-details__group">
-                        <p className="video-details__username">By {VideoMain.channel}</p>
-                        <p className="video-details__date">{relativeDate(VideoMain.timestamp)}</p>
-                    </div>
-                    <div className="video-details__group">
-                        <div className="video-details__views video-details__numbers">
-                            <img className="views__icon" src = {viewsIcon}></img>
-                            <p className="views__number">{VideoMain.views}</p>
-                        </div>
-                        <div className="video-details__likes video-details__numbers">
-                            <img className="likes__icon" src = {likesIcon}></img>
-                            <p className="likes__number">{VideoMain.likes}</p>
-                        </div>
-                    </div>
+  return (
+    <>
+      <video
+        controls
+        className="main-video__video"
+        src={VideoMain.video}
+        poster={VideoMain.image}
+      ></video>
+      <div className="main-video section">
+        <div className="main-video__main-info main-info">
+          <h1 className="main-info__title">{VideoMain.title}</h1>
+          <div className="main-info__video-details">
+            <div className="video-details__stats">
+              <div className="video-details__group">
+                <p className="video-details__username">
+                  By {VideoMain.channel}
+                </p>
+                <p className="video-details__date">
+                  {relativeDate(VideoMain.timestamp)}
+                </p>
+              </div>
+              <div className="video-details__group">
+                <div className="video-details__views video-details__numbers">
+                  <img className="views__icon" src={viewsIcon}></img>
+                  <p className="views__number">{VideoMain.views}</p>
                 </div>
-                <p className = "video-details__description">{VideoMain.description}</p>
+                <div className="video-details__likes video-details__numbers">
+                  <img className="likes__icon" src={likesIcon}></img>
+                  <p className="likes__number">{VideoMain.likes}</p>
+                </div>
+              </div>
             </div>
+            <p className="video-details__description">
+              {VideoMain.description}
+            </p>
+          </div>
         </div>
-    </div>
-
-
-
-)
-
-
-
-
-
-
+      </div>
+    </>
+  );
 }
