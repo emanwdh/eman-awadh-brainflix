19c76a71fe3993b4b6d8fd857989aa5c9da8ce0a
Eman Awadh <emanwdh@gmail.com>
2024-07-21 14:03:44 -0400

    Style and Build: Add name input for comment form, adjust for tablet and desktop media query

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -9,8 +9,12 @@ export default function CommentForm() {
         <span><img className="comment-form__avatar avatar" src = {AvatarImage}/></span>
         <div className="comment-form__input-field">
           <label className="input-field__label">Join the Conversation</label>
-          <textarea className="input-field__textarea" placeholder = "Add a new comment"></textarea>
-          <button className="comment-form__button input-field__button" type="submit">Comment</button>
+          <div className="input-field__inputs">
+            <input placeholder="Add your name"></input>
+            <textarea className="input-field__textarea" placeholder = "Add a new comment"></textarea>
+          </div>
+            <button className="comment-form__button input-field__button" type="submit">Comment</button>
+        
         </div>
       </form>
     </>
