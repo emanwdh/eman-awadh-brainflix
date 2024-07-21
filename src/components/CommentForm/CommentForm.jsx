954b4d84f6a12b5591a149ba9ffd3b883fac331a
Eman Awadh <emanwdh@gmail.com>
2024-07-21 14:31:30 -0400

    Build: Use useState Hook to use controlled form inputs, capture form input using onChange handler

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,8 +1,23 @@
 import MainCommentCounter from "../MainCommentCounter/MainCommentCounter";
 import "./CommentForm.scss"
 import AvatarImage from "../../assets/Images/Mohan-muruge.jpg"
+import { useState } from "react";
 
 export default function CommentForm() {
+  const  [username, setUserName] = useState("");
+  const  [commentText, setCommentText] = useState("");
+
+  function usernameHandler(e) {
+    setUserName(e.target.value);
+    console.log(username);
+  }
+
+  function commentHandler(e) {
+    setCommentText(e.target.value);
+    console.log(commentText);
+  }
+
+
   return (
     <>
       <form className="comment-form">
@@ -10,8 +25,8 @@ export default function CommentForm() {
         <div className="comment-form__input-field">
           <label className="input-field__label">Join the Conversation</label>
           <div className="input-field__inputs">
-            <input placeholder="Add your name"></input>
-            <textarea className="input-field__textarea" placeholder = "Add a new comment"></textarea>
+            <input className="input-field__input"placeholder="Add your name" name="comment-username" value={username} onChange={usernameHandler}></input>
+            <textarea className="input-field__textarea" placeholder = "Add a new comment" name="comment-text" value={commentText} onChange={commentHandler}></textarea>
           </div>
             <button className="comment-form__button input-field__button" type="submit">Comment</button>
         
