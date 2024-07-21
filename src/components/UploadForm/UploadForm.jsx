f0e7ad6d0e3c6cba3ceeb7668f8ba815e0a0db19
Eman Awadh <emanwdh@gmail.com>
2024-07-21 12:20:59 -0400

    Style: Style Upload Page for Tablet and Desktop

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -17,20 +17,26 @@ export default function UploadForm() {
         <section className="upload-section section">
             <h1 className="upload-section__title section__title">Upload Video</h1>
             <form className="upload-form form">
-                <div className="upload-form__add-thumbnail form__input-field">
-                    <label className="upload-form__label">Video Thumbnail</label>
-                    <img className="upload-form__thumbnail" src={Thumbnail}/>
+                <div className="upload-form__upload-options">
+                    <div className="upload-form__add-thumbnail form__input-field">
+                        <label className="upload-form__label">Video Thumbnail</label>
+                        <img className="upload-form__thumbnail" src={Thumbnail}/>
+                    </div>
+                    <div className="upload-form__text-options">
+                        <div className="upload-form__add-title form__input-field">
+                            <label className="upload-form__label ">Title Your Video</label>
+                            <input className="upload-form__input" placeholder="Add a title to your video"></input>
+                        </div>
+                        <div className="upload-form__add-description form__input-field">
+                            <label className="upload-form__label">Add a Video Description</label>
+                            <textarea className="upload-form__text-area" placeholder="Add a description to your video"></textarea>
+                        </div>
+                    </div>
                 </div>
-                <div className="upload-form__add-title form__input-field">
-                    <label className="upload-form__label">Title Your Video</label>
-                    <input className="upload-form__input" placeholder="Add a title to your video"></input>
+                <div className="upload-form__buttons">
+                    <Link className="button--link upload--link" to="/" onClick={UploadHandler}><button className="upload-form__button button--post" type="submit">Publish</button></Link>
+                    <Link className ="cancel--link" to="/"><p className="upload-form__cancel">cancel</p></Link>
                 </div>
-                <div className="upload-form__add-description form__input-field">
-                    <label className="upload-form__label">Add a Video Description</label>
-                    <textarea className="upload-form__text-area" placeholder="Add a description to your video"></textarea>
-                </div>
-                <Link className="button--link" to="/" onClick={UploadHandler}><button className="upload-form__button button--post" type="submit">Publish</button></Link>
-                <Link className ="cancel--link" to="/"><p className="upload-form__cancel">cancel</p></Link>
             </form>
         </section>
         </>
